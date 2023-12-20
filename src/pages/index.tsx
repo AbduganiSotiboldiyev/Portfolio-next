16d88d4e1ed928aaeda298ade105c5cf245da787
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2023-12-21 01:28:44 +0900

    sevices

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,55 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+
+import { About, Hero, Projects } from '@/components'
+import { AboutMe } from '@/interface/aboutme.interface'
+import { PortfolioType } from '@/interface/portfolio.interface'
+import { Project } from '@/interface/projects.interface'
+
+
 import Layout from '@/layout/layout'
+import { PortfolioServece } from '@/services/porfolio-service'
+// import SEO from '@/layout/seo/seo'
 
-const inter = Inter({ subsets: ['latin'] })
+import { Box } from '@mui/material'
 
-export default function Home() {
+import { GetServerSideProps } from 'next'
+import Head from 'next/head'
+
+
+
+const IndexPage = ({apps, aboutmes,project} : HomePageProps) => {
+  
+
   return (
-    <Layout>
-      Salom
-    </Layout>
+  
+      <Layout>
+        
+          <Hero apps={apps}/>
+          <About aboutmes = {aboutmes}/>
+          <Projects project = {project}/>
+      </Layout>
+
   )
+}
+
+export default IndexPage
+
+export const getServerSideProps : GetServerSideProps<HomePageProps> = async () => {
+
+  const apps = await PortfolioServece.getAllApps()
+  const aboutmes = await PortfolioServece.getAboutMe()
+  const project = await PortfolioServece.getProjects()
+  return {
+    props: {
+      apps,
+      aboutmes,
+      project
+    }
+  }
+}
+
+
+interface HomePageProps {
+  apps: PortfolioType[],
+  aboutmes : AboutMe[],
+  project : Project[]
 }
