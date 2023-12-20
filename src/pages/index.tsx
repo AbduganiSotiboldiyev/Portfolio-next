c91933142f54a7f123a94ab78a29e860192853a4
AbduganiSotiboldiyev <91188722+AbduganiSotiboldiyev@users.noreply.github.com>
2023-12-21 02:06:53 +0900

    test2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,56 +1,13 @@
-
-import { About, Hero, Projects } from '@/components'
-import { AboutMe } from '@/interface/aboutme.interface'
-import { PortfolioType } from '@/interface/portfolio.interface'
-import { Project } from '@/interface/projects.interface'
-
-
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
 import Layout from '@/layout/layout'
-import { PortfolioServece } from '@/services/porfolio-service'
-// import SEO from '@/layout/seo/seo'
 
-import { Box } from '@mui/material'
+const inter = Inter({ subsets: ['latin'] })
 
-import { GetServerSideProps } from 'next'
-import Head from 'next/head'
-
-
-
-const IndexPage = ({apps, aboutmes,project} : HomePageProps) => {
-  
-
+export default function Home() {
   return (
-  
-      <Layout>
-        salom
-        
-          {/* <Hero apps={apps}/>
-          <About aboutmes = {aboutmes}/>
-          <Projects project = {project}/> */}
-      </Layout>
-
+    <Layout>
+      Salom
+    </Layout>
   )
-}
-
-export default IndexPage
-
-export const getServerSideProps : GetServerSideProps<HomePageProps> = async () => {
-
-  const apps = await PortfolioServece.getAllApps()
-  const aboutmes = await PortfolioServece.getAboutMe()
-  const project = await PortfolioServece.getProjects()
-  return {
-    props: {
-      apps,
-      aboutmes,
-      project
-    }
-  }
-}
-
-
-interface HomePageProps {
-  apps: PortfolioType[],
-  aboutmes : AboutMe[],
-  project : Project[]
 }
